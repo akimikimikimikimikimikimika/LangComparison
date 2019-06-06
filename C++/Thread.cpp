1e1daad2e3c307093a28ae26745f6c7af028a99c
akimikimikimikimikimikimika <40886798+akimikimikimikimikimikimika@users.noreply.github.com>
2019-06-06 16:36:41 +0900

    LangComparison updated

--- C++/Thread.cpp
+++ C++/Thread.cpp
@@ -9,18 +9,19 @@ using namespace std;
 // 9個のスレッドそれぞれで6回のループ
 	// スレッドの内容を定義するこの関数をヘッダーファイルで定義してはならない
 void mtAction(int m) {
-	for (int n=1;n<7;n++) cout << "|(" << m << "," << n << ")| = " << std::hypot(m,n) << "\r\n";
+	for (int n=1;n<7;n++) printf("|(%d,%d)| = %8.5f\n",m,n,std::hypot(m,n));
+	// cout << << ... で出力する場合,値ごとにバラバラの出力になる
 }
 
 void Thread(){
 
-	cout << "同時に実行可能な数: " << std::thread::hardware_concurrency() << "\r\n";
+	cout << "同時に実行可能な数: " << std::thread::hardware_concurrency() << endl;
 
 	std::vector<std::thread> threads;
 
 	for (int m=1;m<10;m++) threads.push_back(std::thread(mtAction,m));
-	cout << "スレッドの終了を待ちます" << endl;
+	cout << "スレッドの終了を待ちます\r\n";
 	for (std::thread &onethread : threads) onethread.join();
-	cout << "スレッドは終了しました" << endl;
+	cout << "スレッドは終了しました\r\n";
 
 }
